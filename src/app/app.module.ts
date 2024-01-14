9ffdc099b203d32b22681fb76871b8f70cf51ef1
Landvoigt <tim.voigt@ik.me>
2024-01-14 19:13:47 +0100

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllTodosComponent } from './all-todos/all-todos.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllTodosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
